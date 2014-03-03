745872d0848b0101d5c1faf27e2cd0baa47025dc
Arne Strout <arne.strout@icrossing.com>
2014-03-03 13:42:52 -0800

    Creating new repo on github

--- grunt/connect.js
+++ grunt/connect.js
@@ -3,8 +3,15 @@ module.exports={
 		options: {
 			protocol: "http", // Change to "http" for non-secure testing
 			port: 6001,
-			host: '*',
-			base: 'test/www'
+			hostname: '*',
+			base: 'test/www',
+            middleware: function(connect,options,middlewares) {
+            	var ssInclude=require('connect-include');
+                // Here we insert connect-include, use the same pattern to add other middleware
+                middlewares.push(ssInclude('test/www'));
+
+                return middlewares;
+            }
   		}
 	}
 }
