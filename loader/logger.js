f1ff458a421f80bd1bd915d1e2ccf770274fcf4a
Arne Strout <arne.strout@icrossing.com>
2014-07-02 16:25:54 -0700

    Changed console fallback

--- loader/logger.js
+++ loader/logger.js
@@ -1,11 +1,20 @@
-// usage: log('inside coolFunc',this,arguments);
-// http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
-window.log = function(){
-    if({{debug}}){ // turn off log display in production
+if({{debug}}){
+	window.log=function(){
         log.history = log.history || [];   // store logs to an array for reference
         log.history.push(arguments);
         if(this.console){
             console.log( Array.prototype.slice.call(arguments) );
         }
-    }
-};
+    };
+}else{
+    var console = {
+        log : function(){},
+        warn : function(){},
+        error : function(){},
+        dir : function(){},
+        trace : function(){},
+        time : function(){},
+        timeEnd : function(){}
+    };
+    window.log=function(){};
+}
